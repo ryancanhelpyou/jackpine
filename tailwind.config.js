cd8135dbab9eb24aa273ccf89fc1813f1cf26ab8
Ryan King <ryancanhelpyou@gmail.com>
2020-08-30 21:58:47 -0400

    adjust for NWHT brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,20 @@ module.exports = {
     plugins: [
         require('@tailwindcss/typography')
     ],
+    theme: {
+        extend: {
+            colors: {
+                'navy': '#00263e',
+                orange: '#b15533',
+                blue:   '#004b7a',
+                yellow: '#f7a532',
+                green:  '#136f63',
+                gray: {
+                    '800': '#3c3c3d',
+                    '400': '#cfc9c5',
+                    '100': '#f8f4f1',
+                },
+            }
+          }
+    },
 };
