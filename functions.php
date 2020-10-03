f428531763a5caa47fa0da5014c3f8eac19a1fd6
Ryan King <ryancanhelpyou@gmail.com>
2020-10-03 13:44:05 -0400

    blocks: archives, audio, quote, buttons, posts

--- functions.php
+++ functions.php
@@ -83,10 +83,16 @@ class jackpine extends Site
         add_theme_support('menus');
         add_theme_support('post-thumbnails');
         add_theme_support('title-tag');
-        add_theme_support('align-wide');
         add_theme_support('custom-logo');
         add_theme_support('custom-background');
         add_theme_support('customize-selective-refresh-widgets');
+        // Gutenberg support
+        add_theme_support( 'wp-block-styles' );
+        add_theme_support('align-wide');
+        add_theme_support( 'editor-styles' );
+        add_theme_support( 'responsive-embeds' );
+        add_theme_support('experimental-custom-spacing');
+        add_theme_support('experimental-link-color');
         // Add theme support for Custom Header
         $header_args = array(
             'default-image'          => '',
